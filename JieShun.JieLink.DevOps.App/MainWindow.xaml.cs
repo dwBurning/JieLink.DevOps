2fe89b5e27578b94aa2dec5febd591cd5f105d09
dengwei <864108071@qq.com>
2020-07-08 18:27:49 +0800

    [tsk]创建运维工具App源代码仓库

---
+++ JieShun.JieLink.DevOps.App/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using JieShun.JieLink.DevOps.App.ViewModels;
+using Panuon.UI.Silver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JieShun.JieLink.DevOps.App
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : WindowX, IComponentConnector
+    {
+        #region Identity
+        private static IDictionary<string, Type> _partialViewDic;
+        #endregion
+
+        #region Property
+        public MainWindowViewModel ViewModel { get; set; }
+
+        public string Text { get; set; }
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        #region EventHandler
+        private void TvMenu_SelectionChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (!IsLoaded)
+                return;
+
+            var selectedItem = TvMenu.SelectedItem as TreeViewItemModel;
+            var tag = selectedItem.Tag;
+            if (tag.IsNullOrEmpty())
+                return;
+
+            if (_partialViewDic.ContainsKey(tag))
+                ContentControl.Content = Activator.CreateInstance(_partialViewDic[tag]);
+            else
+                ContentControl.Content = null;
+        }
+        #endregion
+
+        private void WindowX_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ JieShun.JieLink.DevOps.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using JieShun.JieLink.DevOps.App.Models;
+using Panuon.UI.Silver.Core;
+using System.Collections.ObjectModel;
+
+namespace JieShun.JieLink.DevOps.App.ViewModels
+{
+    public class MainWindowViewModel : PropertyChangedBase
+    {
+        public MainWindowViewModel()
+        {
+            MenuItems = new ObservableCollection<TreeViewItemModel>()
+            {
+                new TreeViewItemModel("Introduction","Introduction", "\uf05a"),
+                new TreeViewItemModel("Overview","Overview", "\uf0eb"),
+                new TreeViewItemModel("NativeControls","NativeControls", "\uf17a")
+                {
+                     MenuItems = new ObservableCollection<TreeViewItemModel>()
+                     {
+                         new TreeViewItemModel("Button","Button"),
+                         new TreeViewItemModel("TextBox","TextBox"),
+                     }
+                }
+            };
+        }
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set { _searchText = value; NotifyPropertyChanged(); OnSearchTextChanged(); }
+        }
+
+
+        private string _searchText;
+
+        public ObservableCollection<TreeViewItemModel> MenuItems { get; } = new ObservableCollection<TreeViewItemModel>();
+
+        #region Event
+        private void OnSearchTextChanged()
+        {
+            foreach (var item in MenuItems)
+            {
+                ChangeItemVisibility(item);
+            }
+        }
+
+        private bool ChangeItemVisibility(TreeViewItemModel model)
+        {
+            var result = false;
+
+            if (model.Header.ToLower().Contains(SearchText.ToLower()))
+                result = true;
+
+            if (model.MenuItems.Count != 0)
+            {
+                foreach (var item in model.MenuItems)
+                {
+                    var inner = ChangeItemVisibility(item);
+                    result = result ? true : inner;
+                }
+            }
+
+            model.Visibility = result ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+            model.IsExpanded = result;
+            return result;
+        }
+
+        #endregion
+
+    }
+}
