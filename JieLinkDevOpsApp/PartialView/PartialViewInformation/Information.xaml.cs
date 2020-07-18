519cc841354f2c5d3a190f5a99979523f148dd07
dengwei <864108071@qq.com>
2020-07-18 18:58:16 +0800

    [tsk]提交介绍页的代码

--- JieLinkDevOpsApp/PartialView/PartialViewInformation/Information.xaml.cs
+++ JieLinkDevOpsApp/PartialView/PartialViewInformation/Information.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using PartialViewInterface;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -42,6 +43,13 @@ namespace PartialViewInformation
         {
             get
             { return MenuType.None; }
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Hyperlink link = sender as Hyperlink;
+            // 激活的是当前默认的浏览器
+            Process.Start(new ProcessStartInfo(link.NavigateUri.AbsoluteUri));
         }
     }
 }
