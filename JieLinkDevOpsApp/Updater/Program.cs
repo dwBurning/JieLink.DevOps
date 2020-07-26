4af1522a72c04792253f7ddc609deb9556cfe726
zhongfengzero <464055844@qq.com>
2020-07-26 13:47:48 +0800

    [tsk]运维升级工具

---
+++ JieLinkDevOpsApp/Updater/Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using static PInvoke.Kernel32;
+using static PInvoke.AdvApi32;
+using static PInvoke.Userenv;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Diagnostics;
+using JieShun.JieLink.DevOps.Updater.Models;
+using JieShun.JieLink.DevOps.Updater.Utils;
+using System.Security.Principal;
+using System.Windows;
+
+namespace JieShun.JieLink.DevOps.Updater
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //判断有无管理员权限
+            if(!CheckIsAdministrator(args))
+            {
+                Console.WriteLine("程序不是以管理员权限运行，程序退出...");
+                return;
+            }
+            AttachConsole(-1);
+
+            Console.WriteLine("命令行参数:{0}", String.Join(" ", args));
+
+            //CreateUpdateRequestForTest();
+            //CreatePackageInfoForTest();
+
+            UpdateRequest request = UpdateUtils.ParseUpdateRequest(args);
+            if (request != null)
+            {
+                if (CheckNeedUpdate(args, request))
+                {
+                    if (args.Contains("-c"))
+                    {
+                        RunAsConsole(args, request);
+                    }
+                    else
+                    {
+                        RunAsWpfApplication(args, request);
+                    }
+
+                }
+            }
+            else
+            {
+                if (args.Contains("-c"))
+                {
+                    Console.WriteLine("缺少升级所需的参数");
+                    Console.WriteLine("程序退出...");
+                    //Console.WriteLine("按任意键继续...");
+                    //Console.Read();
+                }
+                else
+                {
+                    RunAsWpfApplication(args, request);
+                }
+            }
+        }
+        static void RunAsWpfApplication(string[] args, UpdateRequest request)
+        {
+            App app = new App();
+            app.InitializeComponent();
+            MainWindow windows = new MainWindow();
+            MainWindow.UpdateRequest = request;
+            app.MainWindow = windows;
+            app.Run();
+        }
+        static void RunAsConsole(string[] args, UpdateRequest request)
+        {
+            UniversalUpdater updater = new UniversalUpdater(request);
+            try
+            {
+                updater.Start(UpdateProgress);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        static bool CheckIsAdministrator(string[] args)
+        {
+            //返回true不是有权限，只是界面上提示他无权限
+            if (Debugger.IsAttached)
+            {
+                return true;
+            }
+            else if (args.Contains("-runas"))//加上这个判断，防止无限循环启动
+            {
+                return true;
+            }
+
+            //获得当前登录的Windows用户标示
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            //判断当前登录用户是否为管理员
+            if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                return true;
+            }
+            else
+            {
+                //MessageBox.Show("当前进程无管理员权限!");
+                var newArgs = args.ToList();
+                newArgs.Add("-runas");
+                //创建启动对象
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                //设置运行文件
+                startInfo.FileName = Assembly.GetCallingAssembly().Location;
+                //设置启动参数
+                startInfo.Arguments = String.Join(" ", newArgs);
+                //设置启动动作,确保以管理员身份运行
+                startInfo.Verb = "runas";
+                //如果不是管理员，则启动UAC
+                Process.Start(startInfo);  
+                return false;
+
+            }
+        }
+        static bool CheckNeedUpdate(string[] args, UpdateRequest request)
+        {
+            if(Debugger.IsAttached)
+            {
+                return true;
+            }
+            if (args.Contains("-runnow=1"))
+            {
+                return true;
+            }
+            string programDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Updater\\V1.0.0");
+            if (!AppDomain.CurrentDomain.BaseDirectory.StartsWith(programDataPath))
+            {
+                //不在program data下执行，就不执行升级操作
+                Console.WriteLine("正在复制文件到：" + programDataPath);
+                UpdateUtils.TryReplaceFile(AppDomain.CurrentDomain.BaseDirectory, programDataPath, new List<string> {
+                        Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Temp"),
+                        Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Logs")
+                    });
+                FileInfo fi = new FileInfo(Assembly.GetExecutingAssembly().Location);
+                string executePath = Path.Combine(programDataPath, fi.Name);
+                //重新组织命令行参数
+                var newArgs = args.ToList();
+                newArgs.RemoveAll(x => x.StartsWith("-r") || x.StartsWith("-p"));
+                newArgs.Add("-r=\"" + request.RootPath + "\"");
+                newArgs.Add("-p=\"" + request.PackagePath + "\"");
+                ProcessHelper.StartProcess(executePath, String.Join(" ", newArgs));
+                return false;
+            }
+            return true;
+        }
+        static void UpdateProgress(int progress, string message)
+        {
+            Console.Write("{0}({1}%)", message, progress);
+        }
+        static void CreateUpdateRequestForTest()
+        {
+            //测试
+            UpdateRequest updateRequest = new UpdateRequest();
+            updateRequest.Guid = Guid.NewGuid().ToString();
+            updateRequest.Product = "JSOCT2016";
+            updateRequest.RootPath = @"D:\Program Files (x86)\Jielink";
+            updateRequest.PackagePath = @"D:\迅雷下载\JSOCT2016 V2.6.2 Jielink+智能终端操作平台安装包\obj\JSOCT2016-V2.6.2.zip";
+
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UpdateRequest.json");
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+            var json = JsonConvert.SerializeObject(updateRequest, Formatting.Indented);
+            using (FileStream fs = new FileStream(path, FileMode.Truncate, System.IO.FileAccess.ReadWrite))
+            {
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(json);
+                sw.Close();
+            }
+        }
+
+        static void CreatePackageInfoForTest()
+        {
+            //测试
+            ProgramInfo smartCenter = new ProgramInfo();
+            smartCenter.ExecutablePath = "SmartCenter\\SmartCenter.Host.exe";
+            smartCenter.ProcessName = "SmartCenter.Host.exe";
+            smartCenter.ServiceName = "JsstJieLinkSmartCenter";
+            ProgramInfo smartCenterDaemon = new ProgramInfo();
+            smartCenterDaemon.ExecutablePath = "守护进程\\JSST.JieLink.Daemon.exe";
+            smartCenterDaemon.ProcessName = "JSST.JieLink.Daemon.exe";
+            smartCenterDaemon.ServiceName = "JSST.JieLink.Daemon";
+
+            SubPackage smartCenterPackage = new SubPackage();
+            smartCenterPackage.SubPath = "programfiles\\SmartCenter";
+            smartCenterPackage.TargetPath = "SmartCenter";
+            smartCenterPackage.ExcludeList = new List<string>() {
+                "Config",
+                "update",
+                "Agent.dat",
+                "Logs",
+                "*.config",
+            };
+
+            PackageInfo packageInfo = new PackageInfo();
+            packageInfo.KillProcessList = new List<ProgramInfo>() { smartCenterDaemon, smartCenter };
+            packageInfo.RunProcessList = new List<ProgramInfo>() { smartCenter, smartCenterDaemon };
+            packageInfo.SubPackages = new List<SubPackage>() { smartCenterPackage };
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "JSOCT2016.json");
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+            var json = JsonConvert.SerializeObject(packageInfo, Formatting.Indented);
+            using (FileStream fs = new FileStream(path, FileMode.Truncate, System.IO.FileAccess.ReadWrite))
+            {
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(json);
+                sw.Close();
+            }
+        }
+
+    }
+}
---
+++ JieLinkDevOpsApp/Updater/Utils/UpdateUtils.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using JieShun.JieLink.DevOps.Updater.Models;
+using Newtonsoft.Json;
+namespace JieShun.JieLink.DevOps.Updater.Utils
+{
+    public class UpdateUtils
+    {
+        
+        public static UpdateRequest ParseUpdateRequest(string[] args)
+        {
+            UpdateRequest updateRequest = null;
+            if (args.Contains("-file="))
+            {
+                string requestFile = (args.FirstOrDefault(x => x.StartsWith("-file=")) ?? Guid.NewGuid().ToString()).Replace("-file=", "");
+                return updateRequest=TryParsePackageInfo<UpdateRequest>(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, requestFile));
+            }
+            string product = (args.FirstOrDefault(x => x.StartsWith("-product=")) ?? Guid.NewGuid().ToString()).Replace("-product=", "");
+            string guid = (args.FirstOrDefault(x => x.StartsWith("-guid=")) ?? Guid.NewGuid().ToString()).Replace("-guid=", "");
+            string rootPath = (args.FirstOrDefault(x => x.StartsWith("-root=")) ?? string.Empty).Replace("-root=", "");
+            string packagePath = (args.FirstOrDefault(x => x.StartsWith("-package=")) ?? string.Empty).Replace("-package=", "");
+            if (!string.IsNullOrEmpty(rootPath) && !string.IsNullOrEmpty(packagePath))
+            {
+                updateRequest = new UpdateRequest();
+                updateRequest.Product = product;
+                updateRequest.Guid = guid;
+                updateRequest.RootPath = rootPath;
+                updateRequest.PackagePath = packagePath;
+                return updateRequest;
+            }
+            updateRequest = TryParsePackageInfo<UpdateRequest>(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UpdateRequest.json"));
+            return updateRequest;
+        }
+
+        public static void ExtractZip(string zipFile,string dstDir)
+        {
+            Console.WriteLine("解压文件:{0}", zipFile);
+            ZipHelper.UnzipFile(zipFile, dstDir);
+        }
+        public static T TryParsePackageInfo<T>(string filePath)
+        {
+            try
+            {
+                return ParsePackageInfo<T>(filePath);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("TryParsePackageInfo:" + ex.Message);
+                return default(T);
+            }
+        }
+        public static T ParsePackageInfo<T>(string filePath)
+        {
+            Console.WriteLine("解析升级包json文件:{0}", filePath);
+            if (!File.Exists(filePath))
+            {
+                throw new Exception("文件不存在："+ filePath);
+            }
+            
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                StreamReader sr = new StreamReader(fs);
+                string json = sr.ReadToEnd();
+                sr.Close();
+                var ret= JsonConvert.DeserializeObject<T>(json);
+                if(ret==null)
+                {
+                    throw new Exception("文件解析异常：" + filePath);
+                }
+                return ret;
+            }
+        }
+        public static void KillProcess(List<ProgramInfo> processList,int waitSeconds)
+        {
+            foreach(var info in processList)
+            {
+                Console.WriteLine("结束进程:{0}", info.ProcessName);
+                ProcessHelper.StopProcess(info.ProcessName);
+            }
+            Thread.Sleep(waitSeconds);
+            foreach (var info in processList)
+            {
+                if (string.IsNullOrEmpty(info.ServiceName))
+                {
+                    //服务名为空就代表不是服务，就按进程的方式运行
+                    if (ProcessHelper.IsProcessRunning(info.ProcessName))
+                    {
+                        throw new Exception(info.ProcessName + "关闭失败！");
+                    }
+
+                }
+                else
+                {
+                    if (ProcessHelper.IsServiceRunning(info.ServiceName))
+                    {
+                        throw new Exception(info.ServiceName + "关闭失败！");
+                    }
+                }
+
+            }
+        }
+        public static bool TryReplaceFile(string sourcePath, string dstPath, List<string> ignores)
+        {
+            try
+            {
+                ReplaceFile(sourcePath, dstPath, ignores);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("TryReplaceFile:" + ex.Message);
+                return false;
+            }
+        }
+        public static void ReplaceFile(string sourcePath,string dstPath,List<string> ignores)
+        {
+            //ignores为完整路径
+            if (!Directory.Exists(dstPath))
+                Directory.CreateDirectory(dstPath);
+            var sourceFilePaths = Directory.GetFiles(sourcePath);
+            foreach(var sourceFilePath in sourceFilePaths)
+            {
+                FileInfo fi = new FileInfo(sourceFilePath); 
+                if (ignores.Any(x => IsFileMatch(fi.FullName,fi.Extension,x)))
+                    continue;
+                string dstFilePath = Path.Combine(dstPath,fi.Name);
+                Console.WriteLine("复制文件:{0}", dstFilePath);
+                File.Copy(sourceFilePath, dstFilePath,true);
+            }
+            //递归复制子文件夹
+            var subDirs=Directory.GetDirectories(sourcePath);
+            foreach(var subDir in subDirs)
+            {
+                if (ignores.Contains(subDir))
+                    continue;
+                ReplaceFile(subDir, Path.Combine(dstPath, new DirectoryInfo(subDir).Name), ignores);
+            }
+        }
+        public static bool TryStartProcess(string root, List<ProgramInfo> processList)
+        {
+            try
+            {
+                StartProcess(root, processList);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("TryStartProcess:" + ex.Message);
+                return false;
+            }
+        }
+        public static void StartProcess(string root, List<ProgramInfo> processList)
+        {
+            foreach (var info in processList)
+            {
+                if(string.IsNullOrEmpty(info.ServiceName))
+                {
+                    //服务名为空就代表不是服务，就按进程的方式运行
+                    if(!ProcessHelper.IsProcessRunning(info.ProcessName))
+                    {
+                        string filePath = info.ExecutablePath;
+                        if (File.Exists(info.ExecutablePath))
+                        {
+                            filePath = info.ExecutablePath;
+                        }
+                        else
+                        {
+                            filePath = Path.Combine(root, info.ExecutablePath);
+                        }
+                        Console.WriteLine("启动进程:{0}", info.ProcessName);
+                        ProcessHelper.StartProcess(filePath, info.Args);
+                    }
+                    
+                }
+                else
+                {
+                    if (!ProcessHelper.IsServiceRunning(info.ServiceName))
+                    {
+                        Console.WriteLine("启动服务:{0}", info.ServiceName);
+                        ProcessHelper.StartService(info.ServiceName);
+                    }
+                }
+                
+            }
+            //检查进程是否启动
+            Thread.Sleep(1000);
+            foreach (var info in processList)
+            {
+                if (string.IsNullOrEmpty(info.ServiceName))
+                {
+                    //服务名为空就代表不是服务，就按进程的方式运行
+                    if (!ProcessHelper.IsProcessRunning(info.ProcessName))
+                    {
+                        throw new Exception(info.ProcessName + "启动失败！");
+                    }
+
+                }
+                else
+                {
+                    if (!ProcessHelper.IsServiceRunning(info.ServiceName))
+                    {
+                        throw new Exception(info.ServiceName + "启动失败！");
+                    }
+                }
+
+            }
+        }
+        private static bool IsFileMatch(string currentPath,string extension, string match)
+        {
+            if(string.Equals(currentPath,match,StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            if(match.Contains("*."))
+            {
+                if(string.Equals(match.Substring(match.IndexOf("*.")+1), extension,StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+        public static void TryDeleteDir(string dir)
+        {
+            try
+            {
+                Directory.Delete(dir, true);
+            }
+            catch
+            {
+
+            }
+            
+        }
+    }
+}
