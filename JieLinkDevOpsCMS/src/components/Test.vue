515e34a90be31fa38b217e338e552f34f8215d88
zhongfengzero <464055844@qq.com>
2020-07-30 11:42:46 +0800

    [tsk]修改上传大小限制以及上传接口返回此文件下载的相对路径

--- JieLinkDevOpsCMS/src/components/Test.vue
+++ JieLinkDevOpsCMS/src/components/Test.vue
@@ -9,7 +9,7 @@
             :before-remove="beforeRemove"
             multiple
             :limit="3"
-            :on-exceed="handleExceed"
+            :on-success="handleSuccess"
             :file-list="fileList">
              <el-button size="small" type="primary">点击上传</el-button>
             <!-- <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div> -->
@@ -23,21 +23,23 @@ export default {
   name: 'App',
   data() {
      return {
-       Height: 0
+       Height: 0,
+       fileList:[]
      }
   },
   methods: {
     handleRemove(file, fileList) {
-      console.log(file, fileList);
+      console.log(file,fileList);
     },
     handlePreview(file) {
       console.log(file);
     },
-    handleExceed(files, fileList) {
-      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
-    },
+
     beforeRemove(file, fileList) {
       return this.$confirm(`确定移除 ${ file.name }？`);
+    },
+    handleSuccess(response, file, fileList){
+      console.log('上传成功：下载url='+response);
     }
   }
 }
