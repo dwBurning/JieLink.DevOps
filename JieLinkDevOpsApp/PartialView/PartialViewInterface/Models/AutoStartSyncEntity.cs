a7ee67d1c56d3a0570652f360edcd30702d13a38
gaoguodong <ggdauo@qq.com>
2020-11-02 15:43:40 +0800

    [tsk]增加配置实体类

--- JieLinkDevOpsApp/PartialView/PartialViewInterface/Models/AutoStartSyncEntity.cs
+++ JieLinkDevOpsApp/PartialView/PartialViewInterface/Models/AutoStartSyncEntity.cs
@@ -8,6 +8,32 @@ namespace PartialViewInterface.Models
 {
     public class AutoStartSyncEntity
     {
+        /// <summary>
+        /// 自动开启标志
+        /// </summary>
+        public bool AutoStartFlag { get; set; }
+
+        /// <summary>
+        /// 循环时间
+        /// </summary>
+        public int LoopTime { get; set; }
+
+        /// <summary>
+        /// 查询天数
+        /// </summary>
+        public int Day { get; set; }
+
+        /// <summary>
+        /// 限制条数
+        /// </summary>
+        public int Limit { get; set; }
+
+        /// <summary>
+        /// 是否选择版本控件
+        /// false为2.0以上版本
+        /// true为2.0以下版本    
+        /// </summary>
+        public bool VersionCheck { get; set; }
 
     }
 }
