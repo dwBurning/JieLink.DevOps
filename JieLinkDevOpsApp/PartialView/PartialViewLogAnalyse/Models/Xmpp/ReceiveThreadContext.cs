9f1370514abbf61c880c091f3ef84eceb50e085e
zhongfengzero <464055844@qq.com>
2021-01-14 18:10:08 +0800

    [tsk]Xmpp掉线分析

---
+++ JieLinkDevOpsApp/PartialView/PartialViewLogAnalyse/Models/Xmpp/ReceiveThreadContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialViewLogAnalyse.Models
+{
+    public class ReceiveThreadContext
+    {
+        public string ThreadNum;
+        public string Name;
+        public DateTime StartTime;
+        public DateTime EndTime;
+        public List<ReceiveData> LastLines=new List<ReceiveData>();
+        
+    }
+}
