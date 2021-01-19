583029ac7a6a73ef1697f06fb9e86a0cd700781d
dengwei <864108071@qq.com>
2021-01-19 18:35:23 +0800

    [tsk]更换设备导致场内月租车要收费的问题

--- JieLinkDevOpsApp/PartialView/PartialViewWiki/ViewModels/ChangeDeviceViewModel.cs
+++ JieLinkDevOpsApp/PartialView/PartialViewWiki/ViewModels/ChangeDeviceViewModel.cs
@@ -1,4 +1,6 @@
-﻿using PartialViewWiki.Models;
+﻿using PartialViewInterface.Commands;
+using PartialViewInterface.Utils;
+using PartialViewWiki.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +13,37 @@ namespace PartialViewWiki.ViewModels
     public class ChangeDeviceViewModel : DependencyObject
     {
 
+        public bool CanExecute { get; set; }
+        public DelegateCommand ChangeDeviceCommand { get; set; }
+
         public ChangeDeviceViewModel()
         {
+            ChangeDeviceCommand = new DelegateCommand();
+            ChangeDeviceCommand.ExecuteAction = ChangeDevice;
+            ChangeDeviceCommand.CanExecuteFunc = new Func<object, bool>((object parameter) => { return CanExecute; });
+
             Devices = new List<Device>() {
                 new Device(){
                     DeviceName = "入口",
                     DeviceId = "1",
-                    Devices=new List<Device>()
+                    Devices = new List<Device>()
+                    {
+                        new Device()
+                        {
+                            DeviceName = "出口",
+                            DeviceId = "2"
+                        },new Device()
+                        {
+                            DeviceId = "3",
+                            DeviceName = "入口"
+
+                        }
+                    }
+                },
+                new Device(){
+                    DeviceName = "入口",
+                    DeviceId = "1",
+                    Devices = new List<Device>()
                     {
                         new Device()
                         {
@@ -33,6 +59,36 @@ namespace PartialViewWiki.ViewModels
                 }
             };
         }
+
+        public void SelectedChanged()
+        {
+            if (SelectDevice == null)
+            {
+                CanExecute = false;
+            }
+            else
+            {
+                CanExecute = true;
+            }
+        }
+
+        public void ChangeDevice(object parameter)
+        {
+            MessageBoxHelper.MessageBoxShowInfo(parameter.ToString());
+        }
+
+
+
+        public Device SelectDevice
+        {
+            get { return (Device)GetValue(SelectDeviceIdProperty); }
+            set { SetValue(SelectDeviceIdProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectDeviceId.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectDeviceIdProperty =
+            DependencyProperty.Register("SelectDeviceId", typeof(Device), typeof(ChangeDeviceViewModel));
+
 
 
 
