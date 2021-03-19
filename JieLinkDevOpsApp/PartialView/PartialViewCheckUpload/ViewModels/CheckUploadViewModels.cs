3fc6e1d85961a0521749fffe4f2920553c455861
KanbeKotori <122074995@qq.com>
2021-03-19 11:37:25 +0800

    [tsk]查询上传功能
    保存工作进度

---
+++ JieLinkDevOpsApp/PartialView/PartialViewCheckUpload/ViewModels/CheckUploadViewModels.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using PartialViewCheckUpload.TaskInfos;
+using PartialViewInterface;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PartialViewCheckUpload.ViewModels
+{
+    class CheckUploadViewModels : DependencyObject
+    {
+        public List<TaskInfo> TaskInfos { get; set; }
+
+        private CheckUploadViewModels()
+        {
+            TaskInfos = new List<TaskInfo>();
+
+            GetTaskInfos();
+        }
+
+        public void GetTaskInfos()
+        {
+            try
+            {
+                string sql = $"select table_name from information_schema.`TABLES` where TABLE_SCHEMA='{EnvironmentInfo.DbConnEntity.DbName}';";
+                DataTable dt = MySqlHelper.ExecuteDataset(EnvironmentInfo.ConnectionString, sql).Tables[0];//获取所有的表
+                foreach (DataRow dr in dt.Rows)
+                { 
+                    TaskInfo taskinfo = new TaskInfo { ServiceName = dr["ServiceName"].ToString(),ServiceId = dr["ServiceID"].ToString(), IsChecked = true }
+                    TaskInfos.Add(taskinfo);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
