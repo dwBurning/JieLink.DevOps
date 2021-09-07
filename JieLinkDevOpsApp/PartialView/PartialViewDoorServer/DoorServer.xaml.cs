7f8d44e18f910a9de884d341583361c28f469409
KanbeKotori <122074995@qq.com>
2021-09-07 15:26:00 +0800

    [tsk]增加了门禁服务修复的更多功能
    1.sync_doornum导致的新发卡无法自动下载到设备问题
    2.门禁服务MAC地址配置导致的门禁服务离线问题
    3.HTTP参数不对导致自动下载、完全下载均无法下载人脸到设备问题
    4.Y10A、Y10A_T、Y08A-old搜索显示未知设备的问题

--- JieLinkDevOpsApp/PartialView/PartialViewDoorServer/DoorServer.xaml.cs
+++ JieLinkDevOpsApp/PartialView/PartialViewDoorServer/DoorServer.xaml.cs
@@ -66,45 +66,33 @@ namespace PartialViewDoorServer
             {
                 viewModel.GetDoorServerInfo();
                 this.IsEnabled = true;
-
-                #region 检测卡不能自动下载问题
-                if (viewModel.CheckSyncDoorNum())
-                {
-                    MessageBoxHelper.MessageBoxShowSuccess("检测到sync_doornum数值错误，会导致卡数据无法自动下载！已自动修复！");
-                }
-                #endregion
-
-                #region 检测门禁服务配置文件中MAC地址
-                if (viewModel.CheckDoorServerMac())
-                {
-                    string str = string.Empty;
-                    if (viewModel.IsConfigMac == "true")
-                    {
-                        str = string.Format("检测到门禁服务配置MAC地址错误，CONFIG文件中MAC地址配置为{0}，数据库中门禁服务MAC地址为{1}，是否自动修复？", viewModel.ConfigMAC, viewModel.sqlMac);
-                    }
-                    else
-                    {
-                        str = string.Format("检测到门禁服务配置文件未启用MAC地址配置，可自动配置MAC地址为{0}，是否自动配置？", viewModel.sqlMac);
-                    }
-
-                    if (MessageBoxHelper.MessageBoxShowQuestion(str) == MessageBoxResult.Yes)
-                    {
-                        if(viewModel.FixDoorServerMac())
-                            MessageBoxHelper.MessageBoxShowSuccess("配置门禁服务MAC地址完成！已结束门禁进程等待进程自动重启！");
-                        else
-                            MessageBoxHelper.MessageBoxShowError("配置门禁服务MAC地址失败！");
-                    }
-
-                }
-
-
-                #endregion
             }
             catch (Exception)
             {
                 MessageBoxHelper.MessageBoxShowWarning("请先在【设置】菜单中配置数据库连接");
                 this.IsEnabled = false;
             }
+        }
+
+        /// <summary>
+        /// 右击菜单清屏
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Message = "";
+            //RichTextBox_Text.Document.Blocks.Clear();
+        }
+
+        /// <summary>
+        /// 滚屏
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RichTextBox_Text_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RichTextBox_Text.ScrollToEnd();
         }
     }
 }
